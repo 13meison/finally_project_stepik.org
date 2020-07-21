97c169d99b1267b15bcae85ea5d5614d33f124d6
13meison <13meison@gmail.com>
2020-07-21 08:55:22 +0300

    Добавил метод проверки кнопки логина

--- test_main_page.py
+++ test_main_page.py
@@ -7,7 +7,4 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page.should_be_login_link()
