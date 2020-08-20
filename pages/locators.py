f6bedab1d0fac8a5604b16d6a2006a64609b9093
13meison <13meison@gmail.com>
2020-08-20 11:13:45 +0300

    остановился на задании 4.3.3. Времени нет, перехожу на реальный проект

--- pages/locators.py
+++ pages/locators.py
@@ -12,3 +12,11 @@ class LoginPageLocators():
 
 class ProductPageLocators():
     BTN_ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    FIELD_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    FIELD_NAME_PRODUCT = (By.CSS_SELECTOR, "h1")
+    FIELD_PRICE_ALERT = (By.CSS_SELECTOR, "p.price_color")
+    FIELD_NAME_PRODUCT_ALERT = (By.CSS_SELECTOR, "h1")
+
+    # ALERT_INNER_NAME = (By.CSS_SELECTOR, "#messages .alertinner")
+    # ALERT_INNER_PRICE = (
+    #     By.CSS_SELECTOR, ".#messages:last-child .alertinner")
--- pages/product_page.py
+++ pages/product_page.py
@@ -8,3 +8,27 @@ class ProductPage(BasePage):
         basket_link = self.browser.find_element(
             *ProductPageLocators.BTN_ADD_TO_BASKET)
         basket_link.click()
+
+    def should_be_field_price(self):
+        field_price = self.browser.find_element(
+            *ProductPageLocators.FIELD_PRICE)
+        text_field_price = field_price.text
+        assert self.is_element_present(
+            *ProductPageLocators.FIELD_PRICE), 'price field not found'
+        # реализуйте проверку, что есть форма логина
+
+    def should_be_field_name_product(self):
+        field_name_product = self.browser.find_element(
+            *ProductPageLocators.FIELD_NAME_PRODUCT)
+        text_field_name_product = field_name_product.text
+        assert self.is_element_present(
+            *ProductPageLocators.FIELD_NAME_PRODUCT), 'field name product not found'
+
+    def field_matching_check_name(self):
+        alert_name = self.browser.find_elements(
+            By.CSS_SELECTOR, "#messages .alertinner > strong")
+        text_alert_name = alert_name[0].text
+        text_alert_price = alert_name[2].text
+        assert text_alert_name == text_field_name_product, "jopa"
+        assert text_alert_price == text_field_price, "xyuiuyiu"
+        # assert *ProductPageLocators.FIELD_NAME_PRODUCT.text == text_alert_name, 'field name product not found'
